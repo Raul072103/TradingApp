1dcf2084865cd6eb6a4b5658a45e64e8f8c908b0
raul <raulandrei2017@yahoo.com>
2025-04-25 16:40:07 +0300

    feat(event): order events now contain an inner order struct

--- backend/internal/event/order.go
+++ backend/internal/event/order.go
@@ -1,31 +1,58 @@
 package event
 
+import "errors"
+
+const (
+	BuyOrder  = "BUY_ORDER"
+	SellOrder = "SELL_ORDER"
+)
+
+var ErrOrderTypeIncorrect = errors.New("the order type can be either Buy or Sell")
+
+type Order struct {
+	ID        int64
+	AccountID int64
+	Type      string
+}
+
+func NewOrder(orderType string) (Order, error) {
+	var order Order
+
+	if orderType != BuyOrder && orderType != SellOrder {
+		return order, ErrOrderTypeIncorrect
+	}
+
+	return order, nil
+}
+
 type OrderPlaced struct {
+	order Order
 }
 
 type OrderCanceled struct {
+	order Order
 }
 
 func (e *OrderPlaced) Type() string {
-	return OrdersPlacedEvent
+	return e.order.Type
 }
 
 func (e *OrderPlaced) ID() int64 {
-	return 0
+	return e.order.ID
 }
 
 func (e *OrderPlaced) AccountID() int64 {
-	return 0
+	return e.order.AccountID
 }
 
 func (e *OrderCanceled) Type() string {
-	return OrdersCanceledEvent
+	return e.order.Type
 }
 
 func (e *OrderCanceled) ID() int64 {
-	return 0
+	return e.order.ID
 }
 
 func (e *OrderCanceled) AccountID() int64 {
-	return 0
+	return e.order.AccountID
 }
